6b5cf901fd8fa5f8711818e5b17bcdb33e2369d5
ryan-snyder <36665494+ryan-snyder@users.noreply.github.com>
2024-01-26 13:54:51 +0000

    logging header data

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -3,9 +3,12 @@ import { builder } from '@builder.io/react';
 import type { BuilderContent } from '@builder.io/sdk';
 import { useLoaderData } from '@remix-run/react';
 import type { LoaderFunction } from '@remix-run/node';
+import { BuilderComponentProps } from "@builder.io/react/dist/types/src/components/builder-component.component";
+import { Key } from "react";
 
 
-export default function header({ links, image} : {links : BuilderContent[], image: BuilderContent}) {
+export default function header({ props } : {props: BuilderComponentProps}) {
+    const { links, image } = props.data;
     return (
         <Navbar>
         <NavbarBrand>
@@ -18,10 +21,10 @@ export default function header({ links, image} : {links : BuilderContent[], imag
             <p className="font-bold text-inherit">Kings Playhouse</p>
         </NavbarBrand>
         <NavbarContent className="hidden sm:flex gap-4" justify="center">
-            {links.map((link, index) => (
+            {links.map((link: { url: string, label:string}, index: Key | null | undefined) => (
                 <NavbarItem>
-                    <Link key={index} color="foreground" href={link.data?.url}>
-                        {link.data?.label}
+                    <Link key={index} color="foreground" href={link.url}>
+                        {link.label}
                     </Link>
                 </NavbarItem>
             ))}
