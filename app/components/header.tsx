90aaf4466b402f2a02b0634b92ee6a56191431ba
ryan-snyder <36665494+ryan-snyder@users.noreply.github.com>
2024-01-26 15:37:07 +0000

    adjusting theme color

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,4 +1,5 @@
 import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button, Image} from "@nextui-org/react";
+import { commonColors } from "@nextui-org/react";
 import { builder } from '@builder.io/react';
 import type { BuilderContent } from '@builder.io/sdk';
 import { useLoaderData } from '@remix-run/react';
@@ -11,7 +12,7 @@ interface Link {
 }
 export default function header({ links, image} : {links: [key: Link], image: string}) {
     return (
-        <Navbar>
+        <Navbar isBordered className='bg-green-500'>
         <NavbarBrand>
             <Image
               width={300}
@@ -19,7 +20,6 @@ export default function header({ links, image} : {links: [key: Link], image: str
               alt="Kings Playhouse Logo"
               src={image}
             />
-            <p className="font-bold text-inherit">Kings Playhouse</p>
         </NavbarBrand>
         <NavbarContent className="hidden sm:flex gap-4" justify="center">
             {links.map((link, index) => (
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [nextui],
+  plugins: [nextui({
+    addCommonColors: true
+  })],
 } satisfies Config
-
